cb6222e17d6c489237a4c7e7897c799f93ade6fa
yawayo <38233346+yawayo@users.noreply.github.com>
2019-01-10 16:00:32 +0900

    Update video_to_slomo.py

--- video_to_slomo.py
+++ video_to_slomo.py
@@ -13,15 +13,15 @@ import dataloader
 import platform
 from tqdm import tqdm
 
-# For parsing commandline arguments
+# 명령 실행 시 지정해야 되는 항목들
 parser = argparse.ArgumentParser()
-parser.add_argument("--ffmpeg_dir", type=str, default="", help='path to ffmpeg.exe')
-parser.add_argument("--video", type=str, required=True, help='path of video to be converted')
-parser.add_argument("--checkpoint", type=str, required=True, help='path of checkpoint for pretrained model')
-parser.add_argument("--fps", type=float, default=30, help='specify fps of output video. Default: 30.')
-parser.add_argument("--sf", type=int, required=True, help='specify the slomo factor N. This will increase the frames by Nx. Example sf=2 ==> 2x frames')
-parser.add_argument("--batch_size", type=int, default=1, help='Specify batch size for faster conversion. This will depend on your cpu/gpu memory. Default: 1')
-parser.add_argument("--output", type=str, default="output.mp4", help='Specify output file name. Default: output.mp4')
+parser.add_argument("--ffmpeg_dir", type=str, default="", help='ffmpeg.exe의 경로')
+parser.add_argument("--video", type=str, required=True, help='변환할 비디오의 경로')
+parser.add_argument("--checkpoint", type=str, required=True, help='사전학습된 모델의 체크포인트 경로')
+parser.add_argument("--fps", type=float, default=30, help='출력되는 비디오의 fps 지정. Default: 30.')
+parser.add_argument("--sf", type=int, required=True, help='slomo factor N 지정. Nx만큼 프레임이 증가. 예) sf=2 ==> 2x frames')
+parser.add_argument("--batch_size", type=int, default=1, help='빠른 변환을 위한 batch size 지정. cpu/gpu 메모리에 따라 달라진다. Default: 1')
+parser.add_argument("--output", type=str, default="output.mp4", help='출력되는 비디오의 이름 지정. Default: output.mp4')
 args = parser.parse_args()
 
 def check():
